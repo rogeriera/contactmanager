de25279a14cc8050bd4d146600be0e4145e4f767
Roger Riera Rojas <roger.riera@atrapalo.com>
2018-08-10 09:33:33 +0200

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -9,12 +10,14 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@mail.com" phone="111-111-111" />
-        <Contact
-          name="Karen Williams"
-          email="kwill@mail.com"
-          phone="222-222-222"
-        />
+        <div className="container">
+          <Contact name="John Doe" email="jdoe@mail.com" phone="111-111-111" />
+          <Contact
+            name="Karen Williams"
+            email="kwill@mail.com"
+            phone="222-222-222"
+          />
+        </div>
       </div>
     );
   }
